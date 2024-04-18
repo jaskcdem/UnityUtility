c29b30c71f117bb43d091743782f1d96410708a3
jaskcdem <jaskc2577@gmail.com>
2024-04-18 16:33:28 +0800

    update base-Class

---
+++ FollowCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField] private Transform m_Target = null;
+    [SerializeField] private float m_FollowSpeed = 6.0f;
+    //private void Start()
+    //{
+    //}
+
+    private void FixedUpdate()
+    {
+        if (null == m_Target) return;
+
+        float followSpeed = (m_FollowSpeed * Time.deltaTime);
+        Vector3 pos = this.transform.position;
+        pos = Vector3.Lerp(pos, m_Target.position, followSpeed); //線性內插位置
+        this.transform.position = pos;
+    }
+
+}
